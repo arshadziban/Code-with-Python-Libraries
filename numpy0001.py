4d44e3536b588913ce8a193678bf8c3feffe3a2d
Shah Md. Arshad Rahman Ziban <150821693+arshadziban@users.noreply.github.com>
2024-08-20 22:22:56 +0600

    w3resource

---
+++ numpy0001.py
@@ -0,0 +1,4 @@
+# Write a Numpy program to get the Numpy version and show the Numpy build configuration.
+import numpy as np
+print(np.__version__)
+print(np.show_config())
---
+++ numpy0002.py
@@ -0,0 +1,3 @@
+# Write a NumPy program to get help with the add function.
+import numpy as np
+print(np.info(np.add))
