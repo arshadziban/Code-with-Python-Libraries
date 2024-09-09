a0578fbcde5618c65483888b4b6f3010d3a5dec2
Shah Md. Arshad Rahman Ziban <150821693+arshadziban@users.noreply.github.com>
2024-09-09 23:00:10 +0600

    Linear Algebra

---
+++ numpy0013.py
@@ -0,0 +1,11 @@
+# Write a NumPy program to compute the multiplication of two given matrixes.
+import numpy as np
+p = [[4, 7], [3, 1]]
+q = [[7, 2], [3, 8]]
+
+result1 = np.dot(p, q) 
+# or
+result = np.multiply(p,q)
+
+print (result1)
+print (result)
