e7d1838bf0b84516f0cc33b18864e9a6c061c721
Shah Md. Arshad Rahman Ziban <150821693+arshadziban@users.noreply.github.com>
2024-09-14 15:56:01 +0600

    Linear Algebra

---
+++ numpy0016.py
@@ -0,0 +1,8 @@
+# Write a NumPy program to compute the determinant of a given square array.
+import numpy as np
+# from numpy import linalg as LA
+a = np.array([[1, 0], [1, 2]])
+
+# Compute the determinant of the 2-D array 'a' using np.linalg.det()
+print("Determinant of the said 2-D array:")
+print(np.linalg.det(a)) 
